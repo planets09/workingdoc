9b7ed6c980b4bd49c4d8e2034a249544c64563c6
Rena Tan <rena.tanrx@gmail.com>
2017-06-30 20:52:12 -0700

    exercise

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+
+var dateandTimes = [	
+	{
+		day: 'Monday',
+		time: '9:00 a.m. - 6:00 p.m.'
+	},
+	{
+		day: 'Tuesday',
+		time: 'Closed'
+	},
+	{
+		day: 'Wednesday'.
+		time: '9:30 a.m. - 4:30 p.m.'
+	},
+	{
+		day: 'Thursday',
+		time: '10:00 a.m. - 4:30 p.m.'
+	},
+	{
+		day: 'Friday',
+		time: '9:00 a.m. - 6:00 p.m.'
+	},
+	{
+		day: 'Saturday',
+		time: 'Closed'
+	},
+	{
+		day: 'Sunday',
+		time: 'Closed'
+	}
+];
+
+
+
+
+
+.addEventListener('click')
+function hours(store){
+	for(var i=0; i<dateandTimes.length; i++){
+		if(store.id === dateandTimes[i].day){
+			//need to display day and time?
+		};
+	}
+}
+
