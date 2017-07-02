6325cca76a275b1ab40d92cdbafa45b5c0486e95
Rena Tan <rena.tanrx@gmail.com>
2017-07-02 08:46:17 -0700

    more updates

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var dateandTimes = [
 		time: 'Closed'
 	},
 	{
-		day: 'Wednesday'.
+		day: 'Wednesday',
 		time: '9:30 a.m. - 4:30 p.m.'
 	},
 	{
@@ -31,16 +31,62 @@ var dateandTimes = [
 	}
 ];
 
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+        text = target.textContent || text.innerText;  
 
+        if(target.className === 'btn'){
+        // console.log(target);
+		var hours = document.getElementById("hours");
 
+		switch (text) {
+			case "MON":
+				hours = "The date you selected is: " + dateandTimes.day + " and the open times are: " + dateandTimes.time
+				break;
+			case "TUES":
+				hours = "The date you selected is: " + dateandTimes.day + " and the open times are: " + dateandTimes.time
+				break;
+			case "WED":
+				hours = "The date you selected is: " + dateandTimes.day + " and the open times are: " + dateandTimes.time
+				break;
+			case "THURS":
+				hours = "The date you selected is: " + dateandTimes.day + " and the open times are: " + dateandTimes.time
+				break;
+			case "FRI":
+				hours = "The date you selected is: " + dateandTimes.day + " and the open times are: " + dateandTimes.time
+				break;
+			case "SAT":
+				hours = "The date you selected is: " + dateandTimes.day + " and the open times are: " + dateandTimes.time
+				break;
+			case "SUN":
+				hours = "The date you selected is: " + dateandTimes.day + " and the open times are: " + dateandTimes.time
+				break;
+		}
 
+		if(target.className === 'btn'){
 
-.addEventListener('click')
-function hours(store){
-	for(var i=0; i<dateandTimes.length; i++){
-		if(store.id === dateandTimes[i].day){
-			//need to display day and time?
-		};
+		var store = document.getElementById("store");
+
+		switch (text) {
+			case "MON":
+			store = "OPEN NOW" 
+			break;
+			case "WED":
+			store = "OPEN NOW"
+			break;
+			case "THURS":
+			store = "OPEN NOW"
+			break;
+			case "FRI":
+			store = "OPEN NOW"
+			break;
+		}	
 	}
-}
+
+}, false);
+
+
+
+
 
